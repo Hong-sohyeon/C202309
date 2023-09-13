fa11cb7b50b5d8aaddd67dbdf45327f91a4b263c
hdngso04@naver.com <hdngso04@naver.com>
2023-09-13 16:44:58 +0900

    Week2. 변수, 상수, 자료형, 연산자 심화 실습 + 조서쓰기

---
+++ practice/week2/variable.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int y;
+	int z;
+
+	// int 타입의 x,y,z라는 이름을 가진 변수 생성
+
+	x = 1;
+	y = 2;
+
+	// x,y의 변수에 1,2라는 값 대입
+	
+	z = x + y;
+
+	// 연산기호를 통해 변수 x와 y를 계산한 값을 z에 대입
+
+	printf("%d", z);
+
+	return 0;
+}

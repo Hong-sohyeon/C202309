4835097e03ee6f87cd817ec0b83d7f1618b31e15
hdngso04@naver.com <hdngso04@naver.com>
2023-10-11 16:51:47 +0900

    Week6. 배열

---
+++ practice/week6/studentGrade.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define STUDENTS 5		// 상수 선언
+
+
+// classifyStudents 함수 선언하기
+// scores는 학생 성적을 담는 배열, targetGrade는 출력하려고 하는 특정 성적 입력 
+void classifyStudents(int scores[], char targetGrade) {
+	printf("학생 성적 분류: \n");
+	char grade = ' ';						// 학생 성적 저장 변수
+	for (int i = 0; i < STUDENTS; i++) {	// 학생에게 점수에 따른 성적 부여
+		if (scores[i] >= 90) {
+			grade = 'A';
+		}
+		else if (scores[i] >= 80) {
+			grade = 'B';
+		}
+		else if (scores[i] >= 70) {
+			grade = 'C';
+		}
+		else if (scores[i] >= 60) {
+			grade = 'D';
+		}
+		else {
+			grade = 'F';
+		}
+		if (targetGrade == grade) {			// 출력하려고 하는 성적과 grade 가 일치하는 지 확인
+			printf("%d 학생은 %c 점수를 받았습니다.", i + 1, targetGrade);
+		}
+	}
+}
+// 메인 함수 출력
+int main() {
+	int scores[STUDENTS];	// STUDENTS 상수 만큼의 크기를 가진 scores 배열 생성
+
+	for (int i = 0; i < STUDENTS; i++) {		// 반복문 사용하여 학생들 성적 입력 받기
+		printf("학생 %d의 성적을 입력하세요: ", i + 1);
+		scanf_s("%d", &scores[i]);
+	}
+
+	char ch = getchar(); //버퍼 임시 저장 변수, 엔터 지우기 위해
+
+	char target;
+	printf("특정 점수 (A, B, C, D, F)를 입력하시오:");
+	scanf_s("%c", &target, 1);
+
+	classifyStudents(scores, target);	// classifyStudents 함수 사용하여 학생 성적 분류&특정 성적을 가진 학생 출력
+
+	return 0;
+}
